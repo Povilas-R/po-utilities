1bb65c53b6d4965a41210c380474033fb1b69c2b
Povilas Rūgys <povilas.rugys@gmail.com>
2019-03-17 07:16:46 +0200

    Initial commit.

---
+++ Helper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace Po.Utilities
+{
+    /// <summary>
+    /// Contains various helpful methods.
+    /// </summary>
+    public static class Helper
+    {
+        /// <summary>
+        /// Executes the given action asynchronously using <see cref="Thread"/> class.
+        /// </summary>
+        /// <param name="action">Action to execute.</param>
+        /// <param name="apartmentState"><see cref="Thread"/> instance's apartment state.</param>
+        public static void RunAsync(Action action, ApartmentState apartmentState = ApartmentState.STA)
+        {
+            var thread = new Thread(() =>
+            {
+                action();
+            });
+            thread.SetApartmentState(apartmentState);
+            thread.Start();
+        }
+
+        /// <summary>
+        /// Returns whether the given directory path is valid.
+        /// </summary>
+        public static bool IsDirectoryPathValid(string path)
+        {
+            try
+            {
+                string[] directories = path?.TrimEnd('\\').Split('\\');
+                return
+                    !string.IsNullOrEmpty(path)
+                    && !path.Where(e => Path.GetInvalidPathChars().Contains(e) && e != '\\').Any()
+                    && directories.Skip(1).Where(e => !string.IsNullOrEmpty(e)).Count() == directories.Length - 1
+                    && Directory.Exists(directories[0] + "\\");
+            }
+            catch { }
+            return false;
+        }
+        /// <summary>
+        /// Returns whether the given file name is valid.
+        /// </summary>
+        public static bool IsFileNameValid(string fileName)
+        {
+            try
+            {
+                return
+                    !string.IsNullOrEmpty(fileName)
+                    && !fileName.Where(e => Path.GetInvalidFileNameChars().Contains(e)).Any()
+                    && !fileName.Contains('\\')
+                    && !fileName.Contains(',');
+            }
+            catch { }
+            return false;
+        }
+        /// <summary>
+        /// Returns whether the given file path is valid.
+        /// </summary>
+        public static bool IsFilePathValid(string filePath)
+        {
+            try
+            {
+                string[] levels = filePath?.Split('\\');
+                return
+                    !string.IsNullOrEmpty(filePath)
+                    && IsDirectoryPathValid(string.Concat(levels.Take(levels.Length - 1).Select(e => e + '\\')))
+                    && IsFileNameValid(levels.Last());
+            }
+            catch { }
+            return false;
+        }
+    }
+}
---
+++ UtilityExtensions.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+
+namespace Po.Utilities
+{
+    /// <summary>
+    /// Contains various helpful extension methods.
+    /// </summary>
+    public static class UtilityExtensions
+    {
+        /// <summary>
+        /// Returns double as string without the scientific notation.
+        /// </summary>
+        public static string GetString(this double value)
+        {
+            if (value is double.NaN)
+            {
+                return null;
+            }
+            else
+            {
+                return value.ToString("F99").TrimEnd('0').TrimEnd('.');
+            }
+        }
+
+        /// <summary>
+        /// Reads and returns all new lines from the <see cref="StreamReader"/> buffer.
+        /// </summary>
+        /// <param name="readFromBeginning">True resets the position within the current stream.</param>
+        public static string[] ReadAllLines(this StreamReader streamReader, bool readFromBeginning = false)
+        {
+            if (readFromBeginning)
+            {
+                streamReader.DiscardBufferedData();
+                streamReader.BaseStream.Seek(0, SeekOrigin.Begin);
+            }
+            string[] lines = streamReader.ReadToEnd().Split('\n');
+            if (string.IsNullOrEmpty(lines.Last().TrimEnd('\r')))
+            {
+                return lines.Take(lines.Length - 1).ToArray();
+            }
+            return lines;
+        }
+
+        /// <summary>
+        /// Returns a string of fixed length.
+        /// Adds leading whitespaces.
+        /// </summary>
+        /// <param name="fixedLength">Target length of string.</param>
+        public static string GetFixedString(this int value, int fixedLength)
+        {
+            string result = value.ToString();
+            return
+                string.Concat(string.Empty, Enumerable.Repeat(" ", fixedLength - result.Length).ToArray()) //TODO: test it out
+                + result;
+        }
+        /// <summary>
+        /// Returns a string of fixed length.
+        /// Adds leading whitespaces and trailing zeroes.
+        /// </summary>
+        /// <param name="prePointLength">Target length of string before decimal point.</param>
+        /// <param name="postPointLength">Target number of digits after decimal point.</param>
+        /// <returns></returns>
+        public static string GetFixedString(this double value, int prePointLength, int postPointLength = -1)
+        {
+            if (value is double.NaN)
+            {
+                return string.Concat(Enumerable.Repeat(" ", Math.Max(prePointLength - 3, 0)).ToArray(), "NaN"); //TODO: test it out
+            }
+
+            string result =
+                postPointLength == -1
+                    ? value.GetString()
+                    : Math.Round(value, postPointLength).GetString();
+            string[] split = result.Split('.');
+            if (split.Length == 1)
+            {
+                split = new string[] { split[0], "" };
+            }
+            string extraSpace =
+                (prePointLength - split[0].Length) <= 0
+                ? ""
+                : string.Concat(Enumerable.Repeat(" ", prePointLength - split[0].Length).ToArray());
+            string extraZeroes =
+                postPointLength <= 0
+                ? ""
+                : string.Concat(Enumerable.Repeat("0", postPointLength - split[1].Length).ToArray());
+            return extraSpace + split[0] + (postPointLength <= 0 ? "" : "." + split[1] + extraZeroes);
+        }
+
+        /// <summary>
+        /// Returns the default value of the given <see cref="SettingsProperty"/> from <see cref="SettingsPropertyCollection"/> by the property name.
+        /// </summary>
+        public static type GetDefault<type>(this SettingsPropertyCollection properties, string property)
+        {
+            string str = (string)properties[property].DefaultValue;
+
+            return (type)Convert.ChangeType(str, typeof(type));
+        }
+    }
+}
